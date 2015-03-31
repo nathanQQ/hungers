f632b6f4b81035953f481bcf431385b2a1493ffd
nathanQQ <wuwq85@gmail.com>
2015-03-31 00:39:33 -0700

    support dropbox

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,15 @@
 class Listing < ActiveRecord::Base
   acts_as_paranoid
-  has_attached_file :image, :styles => { :medium => "100x100>", :thumb => "100x100>" }, :default_url => "no-image.jpg"
+  
+  if Rails.env.development?
+  	has_attached_file 	:image, :styles => { :medium => "100x100>", :thumb => "100x100>" }, :default_url => "no-image.jpg"
+  else
+  	has_attached_file 	:image, :styles => { :medium => "100x100>", :thumb => "100x100>" }, :default_url => "no-image.jpg",
+  						:storage => :dropbox,
+  						:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+  						:path => ":styles/:id_:filename"
+  end
+  
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   validates :name, :price, :description, :sold_date, presence: true
   validates :price, numericality: {greater_than: 0}	
