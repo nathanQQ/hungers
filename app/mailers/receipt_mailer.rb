5192ca8beaa64189bd9332191632923d03043399
nathanQQ <wuwq85@gmail.com>
2015-04-14 00:14:35 -0700

    order number test2

--- app/mailers/receipt_mailer.rb
+++ app/mailers/receipt_mailer.rb
@@ -4,8 +4,8 @@ class ReceiptMailer < ActionMailer::Base
   def send_receipt charge
     @charge = charge
     @amount = charge.amount.to_i/100
-    #@id = charge.id.upcase
-    @id = (0...8).map { (65 + rand(26)).chr }.join
+    #@id = (0...8).map { (65 + rand(26)).chr }.join
+    @id = SecureRandom.hex(2).upcase + '-' + SecureRandom.hex(2).upcase
     mail(:to => charge.metadata.email, :subject => "Your Hungers.me order")
   end
 end
