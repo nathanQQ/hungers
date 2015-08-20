636f21c98c10db1ca3e5320f6eab69d15af0b211
nathanQQ <wuwq85@gmail.com>
2015-08-20 00:27:57 -0700

    seller model email confirmable

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -2,7 +2,7 @@ class Seller < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable  #, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
   validates :name, :email, :address_line1, :city, :state, :zip_code, :tax_rate, :phone,:weekday_open_at, :weekday_close_at, :saturday_open_at, :saturday_close_at, :sunday_open_at, :sunday_close_at, presence: true
 
   has_many :listings
