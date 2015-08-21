89e27a35ff7b31fb8a5d765e670614b30c88423d
nathanQQ <wuwq85@gmail.com>
2015-08-20 21:04:16 -0700

    1. production mailer chost set to 'hungers.me'; 2. devise confirmation page beatify

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   
   # action mailer setup
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'hungers.me' }
   config.action_mailer.perform_deliveries = true
   #smtp, sendmail or test
   config.action_mailer.delivery_method = :smtp
